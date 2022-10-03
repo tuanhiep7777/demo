ff399a3e0c658c0c5c7d567d761f17d3b4a968d8
Tuan Hiep Tran <tuanhiep.manucian@gmail.com>
2022-10-03 18:55:06 +0700

    add integration test for CovidRepository

--- src/test/java/com/example/demo/repository/CovidRepositoryIntegrationTest.java
+++ src/test/java/com/example/demo/repository/CovidRepositoryIntegrationTest.java
@@ -1,10 +1,13 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Covid;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
 class CovidRepositoryIntegrationTest {
@@ -15,5 +18,15 @@ class CovidRepositoryIntegrationTest {
     @Test
     public void whenFindById_thenReturnProperEntity() {
 
+        // given
+        Covid covid = new Covid(1L, "Indonesia", null, null, null, null);
+        covidRepository.save(covid);
+
+        // when
+        Covid fromDb = covidRepository.findById(1L).get();
+
+        // then
+        assertThat(fromDb).isNotNull();
+        assertThat(fromDb.getCountry()).isEqualTo(covid.getCountry());
     }
 }
