4029f09b64af9e01c13165ffd89a63cb14bea81f
Tuan Hiep Tran <tuanhiep.manucian@gmail.com>
2022-10-03 18:41:27 +0700

    update formula (2)

--- src/main/java/com/example/demo/repository/CovidRepository.java
+++ src/main/java/com/example/demo/repository/CovidRepository.java
@@ -14,6 +14,7 @@ public interface CovidRepository extends JpaRepository<Covid, Long> {
     @Query("SELECT CASE WHEN :column = 'active' THEN SUM(active) WHEN :column = 'death' THEN SUM(death) END FROM covid")
     Long getTotalOf(String column);
 
-    @Query("SELECT continent AS continent, NVL(SUM(death), 0) / NVL( (SUM(active) + SUM(death) + SUM(recovered)), 1 ) AS impactFactor FROM covid GROUP BY continent")
+    // Excluding case: divide by 0
+    @Query("SELECT continent AS continent, NVL(SUM(death), 0) / (NVL(SUM(active), 0) + NVL(SUM(death), 0) + NVL(SUM(recovered), 0)) AS impactFactor FROM covid GROUP BY continent")
     List<IAggResult> getImpactFactors();
 }
