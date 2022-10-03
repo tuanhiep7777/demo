8fb3e404f0d2826776934562d750269f63a69928
Tuan Hiep Tran <tuanhiep.manucian@gmail.com>
2022-10-03 18:49:56 +0700

    cast result to decimal value

--- src/main/java/com/example/demo/repository/CovidRepository.java
+++ src/main/java/com/example/demo/repository/CovidRepository.java
@@ -14,7 +14,9 @@ public interface CovidRepository extends JpaRepository<Covid, Long> {
     @Query("SELECT CASE WHEN :column = 'active' THEN SUM(active) WHEN :column = 'death' THEN SUM(death) END FROM covid")
     Long getTotalOf(String column);
 
+    // The clause 'AS continent' is required because when not using the alias, Hibernate will generate
+    // a dummy alias for that column, so the result cannot map into IAggResult properly
     // Excluding case: divide by 0
-    @Query("SELECT continent AS continent, NVL(SUM(death), 0) / (NVL(SUM(active), 0) + NVL(SUM(death), 0) + NVL(SUM(recovered), 0)) AS impactFactor FROM covid GROUP BY continent")
+    @Query("SELECT continent AS continent, 1.0 * NVL(SUM(death), 0) / (NVL(SUM(active), 0) + NVL(SUM(death), 0) + NVL(SUM(recovered), 0)) AS impactFactor FROM covid GROUP BY continent")
     List<IAggResult> getImpactFactors();
 }
